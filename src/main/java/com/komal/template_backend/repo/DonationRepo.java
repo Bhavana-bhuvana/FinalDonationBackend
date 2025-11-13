ef4475d5338ebefadec9d030f117f47385731daa
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-11-13 20:19:24 +0530

    Update DonationRepo.java

--- src/main/java/com/komal/template_backend/repo/DonationRepo.java
+++ src/main/java/com/komal/template_backend/repo/DonationRepo.java
@@ -3,5 +3,11 @@ package com.komal.template_backend.repo;
 import com.komal.template_backend.model.Donourentity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface DonationRepo extends MongoRepository<Donourentity,String> {
+    Optional<Donourentity> findByOrderId(String orderId);
+    int deleteByStatusAndDonationDateBefore(String status, LocalDateTime cutoff);
+
 }
